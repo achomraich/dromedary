4e2ad77724f4e609608c3b40fd0b98192f723fa0
yonyawn <jennifer.ikin@kcl.ac.uk>
2024-12-11 16:13:08 +0000

    Fixed minor errors from merge and added sorting to lessons

--- tutorials/management/commands/seed.py
+++ tutorials/management/commands/seed.py
@@ -37,8 +37,8 @@ class Command(BaseCommand):
     """Build automation command to seed the database."""
 
 
-    USER_COUNT = 30
-    LESSON_COUNT = 50
+    USER_COUNT = 10
+    LESSON_COUNT = 15
 
     DEFAULT_PASSWORD = 'Password123'
     help = 'Seeds the database with sample data'
